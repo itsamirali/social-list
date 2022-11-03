666c360daa687a88d41c7be342fd0e9e75aba1a7
Amirali <57327052+itsamirali@users.noreply.github.com>
2022-11-03 12:36:02 +0330

    added header ui

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,7 @@
 import styles from "./Header.module.css";
 import cx from "classnames";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 const Header = () => {
   return (
@@ -8,12 +10,41 @@ const Header = () => {
         styles.headerContainerStyle,
         "d-flex",
         "align-items-center",
-        "justify-content-between"
+        "justify-content-between",
+        "mt-5"
       )}
     >
-      <h2 className={cx(styles.headerTitleStyle, "text-light")}>تنظیمات کاربری</h2>
+      <h2 className={cx(styles.headerTitleStyle, "text-light")}>
+        تنظیمات کاربری
+      </h2>
       <div className="d-flex align-items-center justify-content-center">
-        
+        {/* <WbSunnyIcon className={cx(styles.iconStyle)} /> */}
+        <button
+          className={cx(
+            styles.headerBtnStyle,
+            "py-3",
+            "d-flex",
+            "align-items-center",
+            "justify-content-center"
+          )}
+        >
+          ENGLISH
+        </button>
+        <button
+          className={cx(
+            styles.headerBtnStyle,
+            styles.headerSelectedBtnStyle,
+            "py-3",
+            "d-flex",
+            "align-items-center",
+            "justify-content-center",
+            "ms-4",
+            "me-2"
+          )}
+        >
+          فارسی
+        </button>
+        <DarkModeIcon className={cx(styles.iconStyle)} />
       </div>
     </div>
   );
